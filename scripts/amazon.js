0f789080f91fa85e5e7e37a2be7486c7fac8e7bc
Victor Kambale <kambaleushi@gmail.com>
2026-07-20 16:44:53 +0300

    Use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,38 +2,48 @@
 
 // == ✅✊👌 I. SAVE DATE == //
 // create object inside array
-const products = [
-  // 1 object
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  // 2 object
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  // 3 object
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
+// const products = [
+//   // 1 object
+//   {
+//     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//     rating: {
+//       stars: 4.5,
+//       count: 87,
+//     },
+//     priceCents: 1090,
+//   },
+//   // 2 object
+//   {
+//     image: "images/products/intermediate-composite-basketball.jpg",
+//     name: "Intermediate Size Basketball",
+//     rating: {
+//       stars: 4,
+//       count: 127,
+//     },
+//     priceCents: 2095,
+//   },
+//   // 3 object
+//   {
+//     image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+//     name: "Adults Plain Cotton T-Shirt - 2 Pack",
+//     rating: {
+//       stars: 4.5,
+//       count: 56,
+//     },
+//     priceCents: 799,
+//   },
+//   // 4 object
+//   {
+//     image: "images/products/black-2-slot-toaster.jpg",
+//     name: "Black 2-Slot Toaster",
+//     rating: {
+//       stars: 5,
+//       count: 2197,
+//     },
+//     priceCents: 1899,
+//   },
+// ];
 
 // == ✅✊👌 II. GENERATE HTML [-combine this html together, -put it on the web page] == //
 
@@ -98,4 +108,4 @@ console.log(productsHTML);
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 // === ✅✊👌 III. MAKE IT INTERACTIVE === //
 
-// STOP ON 11:50
+// STOP ON 11:55:00
