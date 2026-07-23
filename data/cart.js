c86eaf9f69917eb3971c9a4c0fe85f02e58fc081
Victor Kambale <kambaleushi@gmail.com>
2026-07-23 20:14:45 +0300

    Save cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,24 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  //
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+// 6 is a part of 6
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    //
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    },
+  ];
+}
+
+// 6 saveToStorage func use localStorage to fix the back product when u refresh the page
+export function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 // func
 export function addToCart(productId) {
@@ -34,6 +44,9 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+
+  // 6  save localStorage
+  saveToStorage();
 }
 
 //5 func to removeFormatCart
@@ -48,4 +61,7 @@ export function removeFormatCart(productId) {
 
   // replace and add new cart now
   cart = newCart;
+
+  // 6  save localStorage
+  saveToStorage();
 }
